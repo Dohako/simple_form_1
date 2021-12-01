462ebe324683e2d1e0fb21a5bddb2e3b67347d73
dohako <dohako@inbox.ru>
2021-12-01 18:30:52 +0300

    code completed

--- main.go
+++ main.go
@@ -8,14 +8,34 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("template/*.html"))
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/process", processor)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func processor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	first_name := r.FormValue("first_name")
+	last_name := r.FormValue("last_name")
+
+	data := struct {
+		First string
+		Last  string
+	}{
+		First: first_name,
+		Last:  last_name,
+	}
+
+	tpl.ExecuteTemplate(w, "processor.html", data)
 }
